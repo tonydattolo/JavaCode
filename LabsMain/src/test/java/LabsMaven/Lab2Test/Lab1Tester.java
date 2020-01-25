57dc36b7b12da3b894bc458a9f2247e65f5ae5b1
tonydattolo <tdattolo@iu.edu>
2020-01-25 17:49:13 -0500

    maven testing

--- LabsMain/src/test/java/LabsMaven/Lab2Test/Lab1Tester.java
+++ LabsMain/src/test/java/LabsMaven/Lab2Test/Lab1Tester.java
@@ -9,13 +9,10 @@ import org.junit.Test;
  */
 public class Lab1Tester
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void test()
-    {
-        
-        assertTrue( true );
+    public void test() {
+        JunitTesting test = new JunitTesting();
+        int output = test.square(5);
+        assertEquals(25,output);        
     }
 }
