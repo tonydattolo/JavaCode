8b7bc7ee308a1177d8f62d66e8e79b92859c3247
tdattolo <tdattolo@iu.edu>
2020-01-27 12:34:50 -0500

    Lab2 Working

--- LabsMain/src/main/java/LabsMaven/Lab2/CompoundExercises.java
+++ LabsMain/src/main/java/LabsMaven/Lab2/CompoundExercises.java
@@ -4,23 +4,31 @@
 //  Released:  1/20/20
 //
 //  Lab 2
-//  @Author  Your Name and username
-//  Last Edited:  
+//  @Author  Tony Dattolo, tdattolo
+//  Last Edited:  1/27/2020
 //
 //
 //  Directions: See Below 
 //               
 //////////////////////////////////////////////////////////////////////////////////
 
+package LabsMaven.Lab2;
+
 // 1. Change the following program to use compound assignments, pre- or post- increment/decrement statements ONLY as appropriate.
 class CompoundExercises {
+
+     // Test output main method
+     public static void main(String[] args) {
+          // compoundOperators();
+          question1();
+     }
 
      public static void compoundOperators(){
           
           int result = -1; // result is -1
           System.out.println(result);
 
-          result = result - 1; // result is now -2
+          --result; // result is now -2
           System.out.println(result);
 
           result = result * -2; // result is now 4
@@ -29,7 +37,7 @@ class CompoundExercises {
           result = result / 2; // result is now 2
           System.out.println(result);
 
-          result = result + 3; // result is now 5
+          result += 3; // result is now 5
           System.out.println(result);
 
           result = result % 3; // result is now 2
@@ -47,4 +55,21 @@ class CompoundExercises {
         System.out.println(i--);  // "-4"
         System.out.println(++i);  // "-4"
      }
+
 }
+
+// ANSWERS
+//
+// 2. -4 is printed thrice in a row because of the way pre/post incrementing/decrementing works.
+// Line 49: i is initialized to -5
+// Line 50: i++; is executed, meaning the next time i is called, it's value will be i + 1
+// Line 51: i is called, the expected -4 is shown
+// Line 52: ++i; is executed, the value of i has changed to -3 because pre-incrementing changes
+//             the value of the variable before its called
+// Line 53: i is called, we see the expected result of -3
+// Line 54: --i is called. Pre-decrementing means the value of i changes before it is called.
+//             We see the expected -4
+// Line 55: i-- is called, meaning the value of i won't decrement until the next call.
+//             called value is still -4
+// Line 56: the value of i here is -5, but because we pre-increment i in this next call,
+//             the value is incremented before i is called, and thus we have -4
--- LabsMain/src/main/java/LabsMaven/Lab2/scrap.java
+++ LabsMain/src/main/java/LabsMaven/Lab2/scrap.java
@@ -4,30 +4,42 @@ import java.lang.StringBuilder;
  * scrap
  */
 public class scrap {
-
     public static void main(String[] args) {
-        // 12317910
-        // long money = 12317910;
-        int money = 777;
         
-        String moneyString = Integer.toString(money);
-        StringBuilder moneyStringCopy = new StringBuilder(moneyString);
-        StringBuilder commas = new StringBuilder("");
-        for (int i=moneyString.length(); i > 3; i-=3) {
-            if (moneyStringCopy.length() > 3) {
-                commas.insert(0, ","+moneyString.substring(i-3,i));
-            }
-            else {
-                commas.insert(0,moneyString.substring(0, i));
-            }
-            moneyStringCopy.delete(i-3, i);
-        }
-        if (moneyStringCopy.length() != 0){
-            commas.insert(0, moneyStringCopy);
-            moneyStringCopy.delete(0, moneyStringCopy.length());
-        }
-        String moneyWithCommas = "$"+commas; 
-        System.out.println(moneyWithCommas);
-        // return moneyWithCommas;
     }
+
 }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// DISCARDED SCRAP
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// 1. addCommas method testing
+
+// public static void main(String[] args) {
+//     // 12317910
+//     // long money = 12317910;
+//     int money = 777;
+    
+//     String moneyString = Integer.toString(money);
+//     StringBuilder moneyStringCopy = new StringBuilder(moneyString);
+//     StringBuilder commas = new StringBuilder("");
+//     for (int i=moneyString.length(); i > 3; i-=3) {
+//         if (moneyStringCopy.length() > 3) {
+//             commas.insert(0, ","+moneyString.substring(i-3,i));
+//         }
+//         else {
+//             commas.insert(0,moneyString.substring(0, i));
+//         }
+//         moneyStringCopy.delete(i-3, i);
+//     }
+//     if (moneyStringCopy.length() != 0){
+//         commas.insert(0, moneyStringCopy);
+//         moneyStringCopy.delete(0, moneyStringCopy.length());
+//     }
+//     String moneyWithCommas = "$"+commas; 
+//     System.out.println(moneyWithCommas);
+//     // return moneyWithCommas;
+// }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
