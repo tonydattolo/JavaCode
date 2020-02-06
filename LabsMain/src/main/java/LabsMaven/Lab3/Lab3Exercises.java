58d881f17c4688d9426499736c3128493e0185e8
tdattolo <tdattolo@iu.edu>
2020-02-05 22:48:50 -0500

    Lab3 Complete

--- LabsMain/src/main/java/LabsMaven/Lab3/Lab3Exercises.java
+++ LabsMain/src/main/java/LabsMaven/Lab3/Lab3Exercises.java
@@ -1,31 +1,73 @@
-/**
- * Lab3Exercises
- */
+////////////////////////////////////////////////////////////////////////////////////
+//  C212
+//   
+//  Released:  1/27/20
+//
+//  Lab 3
+//  @Author  Tony Dattolo, tdattolo
+//  Last Edited:  2/5/2020
+//
+//
+//  Directions: Implement assigned methods and testing as described in Lab3.pdf
+//               
+//////////////////////////////////////////////////////////////////////////////////
 
 package LabsMaven.Lab3;
 
 import java.io.SequenceInputStream;
+import java.lang.ProcessBuilder.Redirect.Type;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Lab3Exercises {
 
     public static void main(String[] args) {
-        // System.out.println(my1089puzzle(371));
-        // System.out.println(my1089puzzle(2));
-        // System.out.println(my1089puzzle(5236435));
-        // System.out.println(my1089puzzle(-2));
-        // System.out.println(my1089puzzle(-23523));
+        // TEST: my1089puzzle method
+        // System.out.println("TEST: my1089puzzle method");
+        // Scanner sc = new Scanner(System.in);
+        // int testPuzzle = sc.nextInt();
+        // System.out.println(my1089puzzle(testPuzzle));
+        // System.out.println("===================");
 
+        // TEST: formatPhoneNumber method
+        // System.out.println("TEST: formatPhoneNumber method");
+        // System.out.println(formatPhoneNumber("5556667777", true));
+        // System.out.println(formatPhoneNumber("5556667777", false));
+        // System.out.println("===================");
 
-        System.out.println(22/10);
-        System.out.println(22.0/7);
-        System.out.println(22/7.0);
+        // TEST: numbersList method
+        // System.out.println("TEST: numbersList method");
+        // System.out.println(numbersList());
+        // System.out.println("===================");
+
+        // TEST: intToHex method
+        // System.out.println("TEST: intToHex method");
+        // System.out.println(intToHex(543));
+        // System.out.println(intToHex(123));
+        // System.out.println(intToHex(17));
+        // System.out.println(intToHex(16));
+        // System.out.println(intToHex(15));
+        // System.out.println("===================");
+
+        // TEST: Question 3, part B
+        // i.
+        // double t = 9.0;
+        // while (Math.abs(t-9.0/t) > .001) {
+        //     t = (9.0/t + t) / 2.0;
+        // }
+        // System.out.println(t);
+        // ii.
+        // System.out.printf("%03d",7);
+        // iii.
+        String s = "Bye Bye Bye";
+        s = s.replace('y', 'e');
+        System.out.println(s);
 
     }
     /**
      * Discard all but last 3 digits
      * reverse the digits
-     * subtract the original input from the reversed (discarding
-     * and minus sign)
+     * subtract the original input from the reversed (discarding the minus sign)
      * reverse the digits of the difference
      * add the difference and reversed difference
      * Example:
@@ -39,18 +81,17 @@ public class Lab3Exercises {
      * @return the sum
      */
     public static int my1089puzzle(int myInput) {
-        System.out.println("===========");
+        
+        // Account for negative numbers input edge case
         int userInput;
         if (myInput >= 0){
             userInput = myInput;
         } else {
             userInput = myInput * -1;
         }
-        System.out.println(userInput);//1
-
+        
+        // Get last 3 digits. Precede numbers smaller than 3 digits with zeroes
         String last3Digits = Integer.toString(userInput);
-        System.out.println("last3: " + last3Digits);
-        System.out.println("len of last 3: " + last3Digits.length());
         for (int i = 0; i < last3Digits.length(); i++){
             System.out.println("index: " + i);
         }
@@ -61,30 +102,155 @@ public class Lab3Exercises {
                 last3Digits = "0" + last3Digits;
             }
         }
-        System.out.println("last3 "+ last3Digits);//2
+
+        // Reverse last 3 digits
         String reversedUserInput = "";
         for (int i = last3Digits.length()-1; i >= 0; i--) {
             reversedUserInput += last3Digits.charAt(i);
         }
-        System.out.println("reversedUserInput" + reversedUserInput);//3
+
+        // Take difference of last 3 digits and its reverse, accounting for negative edge cases
         int difference = Integer.parseInt(last3Digits) - Integer.parseInt(reversedUserInput);
         if (difference < 0) {
             difference *= -1;
         }
-        System.out.println("difference: "+difference);//4
-        String tmp = Integer.toString(difference);
+        // Account for reverse cases that lead to a zero as first or second number.
+        // Example: 300 becomes 3 and not 003
+        String differenceString = Integer.toString(difference);
+        if (differenceString.length() < 3) {
+            while (differenceString.length() < 3) {
+                differenceString = "0" + differenceString;
+            }
+        }
+        // Reverse the difference
         String reversedDifference = "";
-        for (int i = tmp.length()-1; i >= 0; i--) {
-            reversedDifference += tmp.charAt(i);
+        for (int i = differenceString.length()-1; i >= 0; i--) {
+            reversedDifference += differenceString.charAt(i);
         }
-        if (reversedDifference.length() < 3) {
-            last3Digits = "0" + last3Digits;
-        }
-        System.out.println("reversedDifference"+reversedDifference);//5
+        // Convert reverse difference to an integer, and sum with difference
         int newReversedDifference = Integer.parseInt(reversedDifference);
         int sum = difference + newReversedDifference;
-        System.out.println("Sum: "+sum);//6
+        
         return sum;
 
     }
+
+    /**
+     * Takes in a telephone number as a string
+     * Formats it based on boolean value parameter given from user
+     * @param myNumber Reads telephone number
+     * @param option Determines format of number.
+     *      true = (xxx) xxx-xxxx
+     *      false = xxx-xxx-xxxx
+     * @return formatted telephone number
+     */
+    public static String formatPhoneNumber(String myNumber, boolean option){
+        String formattedNumber = "";
+        String areaCode = "";
+        String middleDigits = "";
+        String endDigits = "";
+
+        if (option == true) {
+            areaCode = "(" + myNumber.substring(0, 3) + ")";
+            middleDigits = myNumber.substring(3, 6);
+            endDigits = myNumber.substring(6, myNumber.length());
+            
+            formattedNumber = areaCode + " " + middleDigits + "-" + endDigits;
+        } else {
+            areaCode = myNumber.substring(0, 3);
+            middleDigits = myNumber.substring(3, 6);
+            endDigits = myNumber.substring(6, myNumber.length());
+            
+            formattedNumber = areaCode + "-" + middleDigits + "-" + endDigits;
+        }
+        return formattedNumber; 
+    }
+
+    /**
+     * Take an int from the user between 5-19
+     * Store values
+     * If outside the range 5-19, prompt for another number
+     * If already entered previously, do not include
+     * anything other than an int exits method
+     * @return string of comma separated values
+     */
+    public static String numbersList() {
+        Scanner in = new Scanner(System.in);
+        ArrayList<String> commaSepNumbers = new ArrayList<>();
+        //HashSet would be better. HashSet.contains has constant time complexity
+        System.out.println("Please enter a number between 5-19: ");
+        while (in.hasNextInt()) {
+            System.out.println("Please enter a number between 5-19: ");
+            int userInput = in.nextInt();
+            if (!(userInput < 5 || userInput > 19) && 
+                !(commaSepNumbers.contains(Integer.toString(userInput)))) {
+                commaSepNumbers.add(Integer.toString(userInput));
+            }
+        }
+        in.close();
+
+        // Convert ArrayList to string and add commas
+        String totalNumbers = "";
+        for (int i = 0; i < commaSepNumbers.size(); i++) {
+            if (i == 0){
+                totalNumbers += commaSepNumbers.get(i);
+            } else {
+                totalNumbers += "," + commaSepNumbers.get(i);
+            }
+        }
+        
+        return totalNumbers;
+    }
+
+    /**
+     * Takes an input as char between A-F
+     * Determines your number grade range based on letter grade
+     * @param letterGrade char between A-F
+     * @return String with message saying your grade value range
+     */
+    public static String grade(char letterGrade) {
+        String gradeReport = "";
+        if (letterGrade == 'A') {
+            gradeReport = "Your grade is between a 93.0 and 100";
+        } else if (letterGrade == 'B') {
+            gradeReport = "Your grade is between a 83.0 and 92.9";
+        } else if (letterGrade == 'C') {
+            gradeReport = "Your grade is between a 73.0 and 82.9";
+        } else if (letterGrade == 'D') {
+            gradeReport = "Your grade is between a 83.0 and 93.0";
+        } else if (letterGrade == 'F') {
+            gradeReport = "Your grade is less than 63.0";
+        }
+
+        return gradeReport;
+    }
+    /**
+     * Converts an integer value in base 10 to its hexidecimal value in base 16
+     * Each hexidecimal value is preceded by 0x notation
+     * Numbers <
+     * @param n
+     * @return
+     */
+    public static String intToHex(int n) {
+        String hexidecimal = "0x";
+        if (n < 16) {
+            hexidecimal += ("0" + String.format("%H", n));
+        } else {
+            hexidecimal += (String.format("%H", n));
+        }
+        return hexidecimal;
+    }
+
 }
+
+// Question 3:
+// Part a.
+//      i.      boolean, false
+//      ii.     Integer, 65
+//      iii.    double, 15.1
+//      iv.     String, 524
+// Part b.
+//      i.      .001, incorrect. it was 3.00009155413138.
+//      ii.     007, correct, it was 007. 03d means place 0's before placing the int in the 3rd position
+//      iii.    Bee Bee Bee,  correct. Replacing every char y with char e
+// 
--- LabsMain/src/test/java/LabsMaven/Lab3Test/Lab3Tester.java
+++ LabsMain/src/test/java/LabsMaven/Lab3Test/Lab3Tester.java
@@ -18,8 +18,25 @@ public class Lab3Tester {
         assertEquals(1089, Lab3Exercises.my1089puzzle(543534));
         assertEquals(1089, Lab3Exercises.my1089puzzle(-27123));
         assertEquals(1089, Lab3Exercises.my1089puzzle(-5));
-        
-        
-        
+    }
+
+    @Test
+    public void testFormatNumber() {
+        assertEquals("(555) 666-7777",Lab3Exercises.formatPhoneNumber("5556667777", true));
+        assertEquals("555-666-7777",Lab3Exercises.formatPhoneNumber("5556667777", false));
+    }
+
+    @Test
+    public void testGrade() {
+        assertEquals("Your grade is between a 93.0 and 100", Lab3Exercises.grade('A'));
+        assertEquals("Your grade is less than 63.0", Lab3Exercises.grade('F'));
+    }
+
+    @Test
+    public void testIntToHex() {
+        assertEquals("0x01", Lab3Exercises.intToHex(1));
+        assertEquals("0x0A", Lab3Exercises.intToHex(10));
+        assertEquals("0x162", Lab3Exercises.intToHex(354));
+        assertEquals("0x395", Lab3Exercises.intToHex(917));
     }
 }
